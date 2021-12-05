13e864e87c82eff4536758b41e9f16f78358ef13
Tricia Brooker <grimmi5894@gmail.com>
2021-12-05 09:37:11 -0500

    added route to catch unknown routes and throw error

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 
 const app = express()
 
+
+app.all('/*', (request, response) => {
+  return response.sendStatus(404)
+})
+
+
 app.listen(1337, () => {
   console.log('Listening on 1337') // eslint-disable-line no-console
 })
